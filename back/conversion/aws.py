caa18b34accf003306f21dac7e15207c3ef48cdd
nay <nayeli.guzman@utec.edu.pe>
2025-06-22 02:27:05 -0500

    add: mm

--- back/conversion/aws.py
+++ back/conversion/aws.py
@@ -1,49 +1,18 @@
-import os
-import json
+import subprocess
 
 def lambda_handler(event, context):
-    # Verificar si 'dot' está en /opt/bin
-    print(os.environ["PATH"])
-    os.environ["PATH"] += ":/opt/bin"
-    print(os.environ["PATH"])
-    path_env = os.environ.get("PATH")
-    
-    bin_dir = ':/opt/bin'
     try:
-        path_directories = path_env.split(':')
-
-        for directory in path_directories:
-            print(f"Contenido de {directory}:")
-            if os.path.exists(directory):
-                for file in os.listdir(directory):
-                    print(f"  {file}")
-            else:
-                print("  Este directorio no existe.")
-        print("sssss")
-        
+        # Ejecutar el comando 'dot' para verificar si Graphviz funciona
+        result = subprocess.run(['dot', '-V'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output = result.stdout.decode('utf-8')
         
         return {
             'statusCode': 200,
-            'body': json.dumps({'message': 'Contenido de /opt/bin impreso correctamente.'}),
-            'headers': {'Content-Type': 'application/json'}
+            'body': f"Graphviz version: {output}"
         }
-        
-    except FileNotFoundError:
-        print("gggg")
-        pass
 
-    # Ejecutar el comando dot para verificar su versión
-    result = os.system("dot -V")
-    
-    if result != 0:
+    except Exception as e:
         return {
             'statusCode': 500,
-            'body': json.dumps({'error': 'Graphviz no está disponible o no se encuentra en el PATH.'}),
-            'headers': {'Content-Type': 'application/json'}
+            'body': f"Error ejecutando Graphviz: {str(e)}"
         }
-    
-    return {
-        'statusCode': 200,
-        'body': json.dumps({'message': 'Graphviz está disponible y el diagrama fue generado correctamente.'}),
-        'headers': {'Content-Type': 'application/json'}
-    }
