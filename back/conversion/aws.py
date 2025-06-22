2d005aa1bdb5033161c383af688de389795d8886
nay <nayeli.guzman@utec.edu.pe>
2025-06-21 23:48:18 -0500

    fix: aws

--- back/conversion/aws.py
+++ back/conversion/aws.py
@@ -41,6 +41,8 @@ def lambda_handler(event, context):
         }
 
     code = body["code"]
+    code = "from diagrams import Cluster, Diagram\nfrom diagrams.aws.compute import ECS, EKS, Lambda\nfrom diagrams.aws.database import Redshift\nfrom diagrams.aws.storage import S3\nfrom diagrams.onprem.vcs import Github\nfrom diagrams.onprem.client import Users\n\nwith Diagram(\"Event Processing\", show=False):\n    source = EKS(\"k8s source\")\n    with Cluster (\"Event Flows\"):\n        with Cluster (\"Event Workers\"):\n            workers = [ECS(\"worker1\"),\n                        ECS (\"worker2\"),\n                        ECS (\"worker3\" )]\n            queue = SQS(\"event queue\")\n            with Cluster (\"Processing\"):\n                handlers = [Lambda(\"proc1\"),\n                            Lambda (\"proc2\"),\n                            Lambda (\"proc3\" )]\n    store = S3(\"events store\")\n    dw = Redshift (\"analytics\")\n    source >> workers >> queue >> handlers\n    handlers >> store\n    handlers >> dw"
+
 
     print(code)
 
