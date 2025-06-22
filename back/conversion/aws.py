8783028afb0cd25b2dc26281c171c9684c99d6e0
nay <nayeli.guzman@utec.edu.pe>
2025-06-22 01:18:09 -0500

    fix: aws

--- back/conversion/aws.py
+++ back/conversion/aws.py
@@ -3,8 +3,24 @@ import json
 
 def lambda_handler(event, context):
     # Verificar si 'dot' está en /opt/bin
+    print(os.environ["PATH"])
     os.environ["PATH"] += ":/opt/bin"
+    print(os.environ["PATH"])
     
+    bin_dir = '/opt/bin'
+    try:
+        bin_files = os.listdir(bin_dir)
+        print(f"Contenido de {bin_dir}: {bin_files}")
+        
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'message': 'Contenido de /opt/bin impreso correctamente.'}),
+            'headers': {'Content-Type': 'application/json'}
+        }
+        
+    except FileNotFoundError:
+        pass
+
     # Ejecutar el comando dot para verificar su versión
     result = os.system("dot -V")
     
